58a21192ee5172314a1be53c97dbc85492d6c8d3
Lauren Beatty <lauren.beatty@treasure-data.com>
2020-12-22 17:20:03 -0600

    update firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,12 @@ import 'firebase/firestore';
 // Initalize Firebase.
 // These details will need to be replaced with the project specific env vars at the start of each new cohort.
 var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
+  apiKey: "AIzaSyCUAyVKewVI4hp_1WjTka8iKzWQb1tY-ec",
+  authDomain: "tcl-18-shopping-list.firebaseapp.com",
+  projectId: "tcl-18-shopping-list",
+  storageBucket: "tcl-18-shopping-list.appspot.com",
+  messagingSenderId: "1010208318256",
+  appId: "1:1010208318256:web:ed4b58b8867ec6d141def5"
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
