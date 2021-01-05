1ae904aa58bada981de8a6adb11dae3760478e38
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-05 12:10:21 -0400

    Remove options object and change loading message

--- src/components/couterList/index.js
+++ src/components/couterList/index.js
@@ -7,15 +7,12 @@ import Counter from '../counter';
 
 function CounterList() {
   const query = db.collection('counters');
-  const options = {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  };
-  const [value, loading, error] = useCollection(query, options);
+  const [value, loading, error] = useCollection(query);
 
   return (
     <section>
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
-      {loading && <span>Collection: Loading...</span>}
+      {loading && <span>Counters: Loading...</span>}
       {value && (
         <span>
           {value.docs.map((doc) => (
