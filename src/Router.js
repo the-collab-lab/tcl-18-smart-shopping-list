a34d46031b7ff1a43ce1c3f727cb8517d9311cd6
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-01-11 17:32:37 -0500

    Add Home Route

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import ListView from './components/ListView/index';
 import AddItem from './components/AddItem/index';
 import CounterList from 'components/counterList';
 import Nav from './components/Nav';
+import Home from './pages/Home/index'
 
 function Routes() {
   return (
     <Router>
       <div className="app-container">
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/list-view" component={ListView} />
           <Route exact path="/add-item" component={AddItem} />
           <Route exact path="/counter" component={CounterList} />
