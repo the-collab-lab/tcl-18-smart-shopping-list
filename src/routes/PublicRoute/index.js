bbc7623762d0dda28de96da7f7bb726834930acf
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-13 01:19:07 -0400

    Create PublicRoute component

---
+++ src/routes/PublicRoute/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { checkTokenLocalStorage } from 'lib/localstorage';
+import { Route, Redirect } from 'react-router-dom';
+
+function PublicRoute({ children, ...rest }) {
+  // Check if we have some token
+  const hasToken = checkTokenLocalStorage();
+  // Analysis: Which places can we visit? or Maybe, a redirect?
+  const analysisRoute = ({ location }) => {
+    const conditionToRedirect = hasToken === true && location.pathname === '/';
+    // Visit or redirect?
+    return conditionToRedirect ? (
+      <Redirect
+        to={{
+          pathname: '/list-view',
+          state: { from: location },
+        }}
+      />
+    ) : (
+      children
+    );
+  };
+
+  return <Route {...rest} render={analysisRoute} />;
+}
+
+export default PublicRoute;
