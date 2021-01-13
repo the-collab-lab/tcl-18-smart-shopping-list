088a0ee3f4204fcd998f28358c7f5e2311e26463
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-01-13 14:08:31 -0500

    Simplified code

--- src/lib/localstorage.js
+++ src/lib/localstorage.js
@@ -1,11 +1,7 @@
 function checkTokenLocalStorage() {
   // Get from LocalStorage
-  const tokenValue = localStorage.getItem('Token');
+  return !!localStorage.getItem('Token');
 
-  if (tokenValue !== null && tokenValue !== undefined) {
-    return true;
-  }
-  return false;
 }
 
 export { checkTokenLocalStorage };
