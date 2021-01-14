c5113d4c852023d707378fbac37dbbfcf504e959
Mariangel <mariangelacosta.88@gmail.com>
2021-01-14 18:08:23 -0500

    se agrega validación al formulario

--- src/components/FormProduct/index.jsx
+++ src/components/FormProduct/index.jsx
@@ -10,6 +10,8 @@ export const FormProduct = () => {
 
   const [products, setProduct] = useState(inicialStateProduct);
 
+  const [error, updateError] = useState(false);
+
   const handleInputProduct = (e) => {
     setProduct({
       ...products,
@@ -20,58 +22,68 @@ export const FormProduct = () => {
   const handleSubmitProduct = (e) => {
     e.preventDefault();
     let { item, nextPurchase, LastPurchasedDate } = products;
+
+    if (item.trim() === '' || nextPurchase === 0) {
+      updateError(true);
+      return;
+    }
+
+    updateError(false);
+
     const editedProduct = {
       item,
       nextPurchase: Number(nextPurchase),
       LastPurchasedDate,
     };
     addProduct(editedProduct);
-    console.log(editedProduct);
     setProduct({ ...inicialStateProduct });
   };
 
   return (
-    <form onSubmit={handleSubmitProduct}>
-      <div>
-        <label>Item name: </label>
-        <input
-          onChange={handleInputProduct}
-          type="text"
-          placeholder="Add A Product"
-          autoComplete="off"
-          name="item"
-          value={products.item}
-        />
-      </div>
-      <div>
-        <label>How soon will you buy this again?</label>
-        <input
-          onChange={handleInputProduct}
-          type="radio"
-          name="nextPurchase"
-          value={7}
-        />
-        Soon
-        <br />
-        <input
-          onChange={handleInputProduct}
-          type="radio"
-          name="nextPurchase"
-          value={14}
-        />
-        Kind Of Soon
-        <br />
-        <input
-          onChange={handleInputProduct}
-          type="radio"
-          name="nextPurchase"
-          value={30}
-        />
-        Not Soon
-        <br />
-      </div>
+    <div>
+      {error ? <p>All fields are required</p> : null}
+      <form onSubmit={handleSubmitProduct}>
+        <div>
+          <label>Item name: </label>
+          <input
+            onChange={handleInputProduct}
+            type="text"
+            placeholder="Add A Product"
+            autoComplete="off"
+            name="item"
+            value={products.item}
+          />
+        </div>
+        <div>
+          <label>How soon will you buy this again?</label>
+          <input
+            onChange={handleInputProduct}
+            type="radio"
+            name="nextPurchase"
+            value={7}
+          />
+          Soon
+          <br />
+          <input
+            onChange={handleInputProduct}
+            type="radio"
+            name="nextPurchase"
+            value={14}
+          />
+          Kind Of Soon
+          <br />
+          <input
+            onChange={handleInputProduct}
+            type="radio"
+            name="nextPurchase"
+            value={30}
+          />
+          Not Soon
+          <br />
+        </div>
 
-      <button type="submit">Add Item</button>
-    </form>
+        <button type="submit">Add Item</button>
+      </form>
+    </div>
   );
 };
