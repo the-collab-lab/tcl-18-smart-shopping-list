3a3b27222cb4d8442f105e7a650ddf97c573144a
Cristina <crisruedap@gmail.com>
2021-01-14 11:34:53 -0500

    Refactor components and update routes

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './pages/Home';
 import ListView from './components/ListView/index';
 import AddItem from './components/AddItem/index';
 import Nav from './components/Nav';
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CheckToken from 'components/CheckToken';
+
+const Home = () => (
+  <section className="Home">
+    <CheckToken />
+  </section>
+);
+export default Home;
