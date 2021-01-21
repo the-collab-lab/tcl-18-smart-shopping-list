770703855410cbc2c7fc7270e0f7bdd091db6624
Mariangel <mariangelacosta.88@gmail.com>
2021-01-21 12:00:06 -0500

    productExists function is extracted to an external file

--- src/components/Utils/firestore.js
+++ src/components/Utils/firestore.js
@@ -10,3 +10,10 @@ export const getProducts = () => {
   const token = localStorage.getItem('tcl18-token');
   return db.collection(token);
 };
+
+export const productExists = (products, normalizedItemInput) => {
+  const normalizedItemsDb = products.docs.map((doc) =>
+    doc.data().item.normalizeItem(),
+  );
+  return normalizedItemsDb.includes(normalizedItemInput);
+};
