9d1257f8d6c224e2f8fd3df92df0ff5961e8952d
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-01-27 18:39:34 -0500

    Use of useEffect so that the item should be shown as checked for 24 hours

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { updateItemDate } from '../Utils/firestore';
+import { differenceInHours } from 'date-fns';
 
-function ItemList({ itemName, docId }) {
+function ItemList({ itemName, docId, lastPurchasedDate }) {
   const [isChecked, setIsChecked] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
   const [token] = useState(() => window.localStorage.getItem('tcl18-token'));
   const toggle = () => setIsChecked(!isChecked);
-
+ 
   const editProduct = (id) => {
     updateItemDate(token, id).then(() => console.log('product updated'));
   };
+  
+  useEffect(() => {
+    if(isChecked) {
+      updateItemDate(token, docId).then(() => console.log('La fecha fue actualizada'))
+    }
+  },[isChecked, docId, token])
 
-  //TODO
-  // crear un metodo que tome la fecha y comparala con la fecha actual
-  // condicional que bloque o desbloquee
+  useEffect(() => {
+    if (lastPurchasedDate && isDisabled===false) {
+      const hoursAtTheMoment = differenceInHours(
+          new Date (), 
+          lastPurchasedDate.toDate());
+          // console.log('hoursAtTheMoment', hoursAtTheMoment, lastPurchasedDate.toDate());
+      setIsDisabled(hoursAtTheMoment <= 24)
+      setIsChecked(hoursAtTheMoment <= 24)
+    }
+  },[isDisabled, lastPurchasedDate])
 
   const handleCheckbox = (event) => {
     toggle();
     if (event.target.checked) {
       editProduct(docId);
       setIsDisabled(true);
-    }
+    } 
   };
 
   return (
