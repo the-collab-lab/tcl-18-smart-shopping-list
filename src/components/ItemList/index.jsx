2cb36eaa7cfdfe38dcd7e5e07c1942e2d0ef79fd
Cristina <crisruedap@gmail.com>
2021-01-28 20:23:45 -0500

    Add error handling in function.

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -7,34 +7,42 @@ function ItemList({ itemName, docId, lastPurchasedDate }) {
   const [isDisabled, setIsDisabled] = useState(false);
   const [token] = useState(() => window.localStorage.getItem('tcl18-token'));
   const toggle = () => setIsChecked(!isChecked);
- 
+
   const editProduct = (id) => {
-    updateItemDate(token, id).then(() => console.log('product updated'));
+    updateItemDate(token, id)
+      .then(() => {
+        console.log('product updated');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-  
-  useEffect(() => {
-    if(isChecked) {
-      updateItemDate(token, docId).then(() => console.log('The date was updated'))
-    }
-  },[isChecked, docId, token])
 
   useEffect(() => {
-    if (lastPurchasedDate && isDisabled===false) {
-      const hoursAtTheMoment = differenceInHours(
-          new Date (), 
-          lastPurchasedDate.toDate());
-          // console.log('hoursAtTheMoment', hoursAtTheMoment, lastPurchasedDate.toDate());
-      setIsDisabled(hoursAtTheMoment <= 12)
-      setIsChecked(hoursAtTheMoment <= 12)
+    if (isChecked) {
+      updateItemDate(token, docId).then(() =>
+        console.log('The date was updated'),
+      );
     }
-  },[isDisabled, lastPurchasedDate])
+  }, [isChecked, docId, token]);
+
+  useEffect(() => {
+    if (lastPurchasedDate && isDisabled === false) {
+      const hoursAtTheMoment = differenceInHours(
+        new Date(),
+        lastPurchasedDate.toDate(),
+      );
+      setIsDisabled(hoursAtTheMoment <= 12);
+      setIsChecked(hoursAtTheMoment <= 12);
+    }
+  }, [isDisabled, lastPurchasedDate]);
 
   const handleCheckbox = (event) => {
     toggle();
     if (event.target.checked) {
       editProduct(docId);
       setIsDisabled(true);
-    } 
+    }
   };
 
   return (
