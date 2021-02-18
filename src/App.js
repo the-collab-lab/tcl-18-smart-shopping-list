658feeee9ad480d0a6d387ba5a003fb44d1551eb
Alejandro M. Coca <alxmcr@outlook.com>
2021-02-18 17:32:27 -0400

    Apply styles nav bottom and top

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import AppRoutes from 'routes/AppRoutes';
-import Nav from 'components/Nav';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="main-title">
+        <div className="App-Navbar-Top">
           <h1>Smart Shopping List</h1>
         </div>
         <main className="container mt-5 pt-2">
           <AppRoutes />
-          <Nav />
         </main>
       </BrowserRouter>
     </div>
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,16 +5,30 @@ import './nav.css';
 
 function Nav() {
   return (
-    <nav className="navigator fixed-bottom p-3">
-      <ul>
-        <li>
-          <NavLink to="/list-view" activeClassName="selected">
-            List
+    <nav className="App-Navbar-Bottom fixed-bottom">
+      <ul className="nav nav-pills nav-fill">
+        <li className="nav-item border border-1 border-end border-white">
+          <NavLink
+            to="/list-view"
+            activeClassName="selected"
+            className="nav-link p-2"
+          >
+            <span className="nav-item-content">
+              <i className="bi bi-list-check nav-icon"></i>
+              <span className="nav-text">List</span>
+            </span>
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/add-item" activeClassName="selected">
-            Add Item
+        <li className="nav-item border">
+          <NavLink
+            to="/add-item"
+            activeClassName="selected"
+            className="nav-link p-2"
+          >
+            <span className="nav-item-content">
+              <i className="bi bi-cart-plus-fill nav-icon"></i>
+              <span className="nav-text">Add Item</span>
+            </span>
           </NavLink>
         </li>
       </ul>
